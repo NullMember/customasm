bc45599e543cddaa000b84b186cdaa9e9fccfe47
Malik Enes Şafak <e.maliksafak@gmail.com>
2021-07-14 13:57:46 +0300

    Update VIRTUAL16.asm

--- rules/VIRTUAL16.asm
+++ rules/VIRTUAL16.asm
@@ -48,14 +48,14 @@
 
 #ruledef
 {
-	RET								=> 0x00
-	RTS								=> 0x01
-	CLC								=> 0x02
-	SEC								=> 0x03
+	RET				=> 0x00
+	RTS				=> 0x01
+	CLC				=> 0x02
+	SEC				=> 0x03
 
-	MOV  {src: reg}, {dst: reg}		=> 0x04 @ src`4 @ dst`4
+	MOV  {src: reg}, {dst: reg}	=> 0x04 @ src`4 @ dst`4
 	MOV  @{src: reg}, {dst: reg}	=> 0x05 @ src`4 @ dst`4
-	MOV	 {src: reg}, @{dst: reg}	=> 0x06 @ src`4 @ dst`4
+	MOV  {src: reg}, @{dst: reg}	=> 0x06 @ src`4 @ dst`4
 	MOV  +@{src: reg}, {dst: reg}	=> 0x07 @ src`4 @ dst`4
 	MOV  {src: reg}, +@{dst: reg}	=> 0x08 @ src`4 @ dst`4
 	MOV  {src: reg}.H, {dst: reg}.H	=> 0x09 @ src`4 @ dst`4
@@ -63,63 +63,63 @@
 	MOV  {src: reg}.L, {dst: reg}.H	=> 0x0B @ src`4 @ dst`4
 	MOV  {src: reg}.L, {dst: reg}.L	=> 0x0C @ src`4 @ dst`4
 	SWAP {src: reg}, {dst: reg} 	=> 0x0D @ src`4 @ dst`4
-	SWAP {reg: reg}		 			=> 0x0D @ reg`4 @ reg`4
+	SWAP {reg: reg}		 	=> 0x0D @ reg`4 @ reg`4
 
-	AND  {dst: reg}, {src: reg}		=> 0x0E @ dst`4 @ src`4
-	OR   {dst: reg}, {src: reg}		=> 0x0F @ dst`4 @ src`4
-	XOR  {dst: reg}, {src: reg}		=> 0x10 @ dst`4 @ src`4
+	AND  {src: reg}, {dst: reg}	=> 0x0E @ dst`4 @ src`4
+	OR   {src: reg}, {dst: reg}	=> 0x0F @ dst`4 @ src`4
+	XOR  {src: reg}, {dst: reg}	=> 0x10 @ dst`4 @ src`4
 
-	ADD  {dst: reg}, {src: reg}		=> 0x11 @ dst`4 @ src`4
-	ADC  {dst: reg}, {src: reg}		=> 0x12 @ dst`4 @ src`4
-	SUB  {dst: reg}, {src: reg}		=> 0x13 @ dst`4 @ src`4
-	SBC  {dst: reg}, {src: reg}		=> 0x14 @ dst`4 @ src`4
-	SMUL {dst: reg}, {src: reg}		=> 0x15 @ dst`4 @ src`4
-	UMUL {dst: reg}, {src: reg}		=> 0x16 @ dst`4 @ src`4
+	ADD  {src: reg}, {dst: reg}	=> 0x11 @ dst`4 @ src`4
+	ADC  {src: reg}, {dst: reg}	=> 0x12 @ dst`4 @ src`4
+	SUB  {src: reg}, {dst: reg}	=> 0x13 @ dst`4 @ src`4
+	SBC  {src: reg}, {dst: reg}	=> 0x14 @ dst`4 @ src`4
+	SMUL {src: reg}, {dst: reg}	=> 0x15 @ dst`4 @ src`4
+	UMUL {src: reg}, {dst: reg}	=> 0x16 @ dst`4 @ src`4
 
-	CMP  {dst: reg}, {src: reg}		=> 0x17 @ dst`4 @ src`4
-	CMP  {dst: reg}					=> 0x17 @ dst`4 @ 13`4
+	CMP  {src: reg}, {dst: reg}	=> 0x17 @ dst`4 @ src`4
+	CMP  {src: reg}			=> 0x17 @ dst`4 @ 13`4
 
-	INC  {dst: reg}, #{count}		=> 0x18 @ dst`4 @ count`4
-	INC  {dst: reg}					=> 0x18 @ dst`4 @ 0`4
-	DEC  {dst: reg}, #{count}		=> 0x19 @ dst`4 @ count`4
-	DEC  {dst: reg}					=> 0x19 @ dst`4 @ 0`4
+	INC  {dst: reg}, {count: u4}	=> 0x18 @ dst`4 @ count`4
+	INC  {dst: reg}			=> 0x18 @ dst`4 @ 0`4
+	DEC  {dst: reg}, {count: u4}	=> 0x19 @ dst`4 @ count`4
+	DEC  {dst: reg}			=> 0x19 @ dst`4 @ 0`4
 
-	ASR  {dst: reg}, #{count}		=> 0x1A @ dst`4 @ count`4
-	ASR  {dst: reg}					=> 0x1A @ dst`4 @ 0`4
-	LSL  {dst: reg}, #{count}		=> 0x1B @ dst`4 @ count`4
-	LSL  {dst: reg}					=> 0x1B @ dst`4 @ 0`4
-	LSR  {dst: reg}, #{count}		=> 0x1C @ dst`4 @ count`4
-	LSR  {dst: reg}					=> 0x1C @ dst`4 @ 0`4
-	ROL  {dst: reg}, #{count}		=> 0x1D @ dst`4 @ count`4
-	ROL  {dst: reg}					=> 0x1D @ dst`4 @ 0`4
-	RLC  {dst: reg}, #{count}		=> 0x1E @ dst`4 @ count`4
-	RLC  {dst: reg}					=> 0x1E @ dst`4 @ 0`4
-	ROR  {dst: reg}, #{count}		=> 0x1F @ dst`4 @ count`4
-	ROR  {dst: reg}					=> 0x1F @ dst`4 @ 0`4
-	RRC  {dst: reg}, #{count}		=> 0x20 @ dst`4 @ count`4
-	RRC  {dst: reg}					=> 0x20 @ dst`4 @ 0`4
+	ASR  {dst: reg}, {count: u4}	=> 0x1A @ dst`4 @ count`4
+	ASR  {dst: reg}			=> 0x1A @ dst`4 @ 0`4
+	LSL  {dst: reg}, {count: u4}	=> 0x1B @ dst`4 @ count`4
+	LSL  {dst: reg}			=> 0x1B @ dst`4 @ 0`4
+	LSR  {dst: reg}, {count: u4}	=> 0x1C @ dst`4 @ count`4
+	LSR  {dst: reg}			=> 0x1C @ dst`4 @ 0`4
+	ROL  {dst: reg}, {count: u4}	=> 0x1D @ dst`4 @ count`4
+	ROL  {dst: reg}			=> 0x1D @ dst`4 @ 0`4
+	RLC  {dst: reg}, {count: u4}	=> 0x1E @ dst`4 @ count`4
+	RLC  {dst: reg}			=> 0x1E @ dst`4 @ 0`4
+	ROR  {dst: reg}, {count: u4}	=> 0x1F @ dst`4 @ count`4
+	ROR  {dst: reg}			=> 0x1F @ dst`4 @ 0`4
+	RRC  {dst: reg}, {count: u4}	=> 0x20 @ dst`4 @ count`4
+	RRC  {dst: reg}			=> 0x20 @ dst`4 @ 0`4
 
-	JMPR {reg: reg}					=> 0x21 @ reg`4 @ 15`4
+	JMPR {reg: reg}			=> 0x21 @ reg`4 @ 15`4
 
-	JSR	 {label}					=> 0x22 @ label[7:0] @ label[15:8]
-	NJSR {label}					=> 0x23 @ label[7:0] @ label[15:8]
-	JMP	 {label}					=> 0x24 @ label[7:0] @ label[15:8]
+	JSR	{addr: u16}		=> 0x22 @ addr[7:0] @ addr[15:8]
+	NJSR 	{addr: u16}		=> 0x23 @ addr[7:0] @ addr[15:8]
+	JMP	{addr: u16}		=> 0x24 @ addr[7:0] @ addr[15:8]
 
-	BCC	{label}						=>
+	BCC	{label}			=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0x25 @ reladdr`8
 	}
-	BCS	{label}						=>
+	BCS	{label}			=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0x26 @ reladdr`8
 	}
-	BRA	{label}						=>
+	BRA	{label}			=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
@@ -128,91 +128,91 @@
 	}
 	
 	
-	PUSH {reg: reg}					=> 0x3  @ reg`4
-	POP  {reg: reg}					=> 0x4  @ reg`4
-	MOV  #{value}, {dst: reg}.L		=> 0x5	@ dst`4 @ value`8
-	MOV  #{value}, {dst: reg}.H		=> 0x6	@ dst`4 @ value`8
-	MOV  #{value}, {dst: reg}		=> 0x7	@ dst`4 @ value[7:0] @ value[15:8]
-	MOV	 {label}, {reg: reg}		=> 0x8  @ reg`4 @ label[7:0] @ label[15:8]
-	MOV	 {reg: reg}, {label}		=> 0x9  @ reg`4 @ label[7:0] @ label[15:8]
-	BNM1 {dst: reg}, {label}		=>
+	PUSH {reg: reg}			=> 0x3  @ reg`4
+	POP  {reg: reg}			=> 0x4  @ reg`4
+	MOV  #{value: u8}, {dst: reg}.L	=> 0x5	@ dst`4 @ value`8
+	MOV  #{value: u8}, {dst: reg}.H	=> 0x6	@ dst`4 @ value`8
+	MOV  #{value: u16}, {dst: reg}	=> 0x7	@ dst`4 @ value[7:0] @ value[15:8]
+	MOV  {addr}, {reg: reg}		=> 0x8  @ reg`4 @ addr[7:0] @ addr[15:8]
+	MOV  {reg: reg}, {addr}		=> 0x9  @ reg`4 @ addr[7:0] @ addr[15:8]
+	BNM1 {dst: reg}, {label}	=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xA @ dst`4 @ reladdr`8
 	}
-	BM1	 {dst: reg}, {label}		=>
+	BM1	 {dst: reg}, {label}	=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xB @ dst`4 @ reladdr`8
 	}
-	BMI	 {dst: reg}, {label}		=>
+	BMI	 {dst: reg}, {label}	=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xC @ dst`4 @ reladdr`8
 	}
-	BPL	 {dst: reg}, {label}		=>
+	BPL	 {dst: reg}, {label}	=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xD @ dst`4 @ reladdr`8
 	}
-	BNE	 {dst: reg}, {label}		=>
+	BNE	 {dst: reg}, {label}	=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xE @ dst`4 @ reladdr`8
 	}
-	BEQ	 {dst: reg}, {label}		=>
+	BEQ	 {dst: reg}, {label}	=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xF @ dst`4 @ reladdr`8
 	}
-	BNM1 {label}					=>
+	BNM1 	{label}			=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xAD @ reladdr`8
 	}
-	BM1	 {label}					=>
+	BM1	 {label}		=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xBD @ reladdr`8
 	}
-	BMI	 {label}					=>
+	BMI	 {label}		=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xCD @ reladdr`8
 	}
-	BPL	 {label}					=>
+	BPL	 {label}		=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xDD @ reladdr`8
 	}
-	BNE	 {label}					=>
+	BNE	 {label}		=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
 		assert(reladdr >= !0x7f)
 		0xED @ reladdr`8
 	}
-	BEQ	 {label}					=>
+	BEQ	 {label}		=>
 	{
 		reladdr = label - $ - 2
 		assert(reladdr <=  0x7f)
@@ -224,8 +224,8 @@
 ;Write your code below here
 
 MAIN:
-    
+
 LOOP:
 
 END:
-    RET
+	RET
